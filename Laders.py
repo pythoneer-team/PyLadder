c237e905db195908a3300f1180d4d7694e03eb87
Maram-Ankir <maramankir5@gmail.com>
2021-08-23 14:57:33 +0300

    feat:check laders

---
+++ Laders.py
@@ -0,0 +1,20 @@
+# Ladder check
+def ladders(x):
+    if x == 1:
+        return 38
+    elif x == 4:
+        return 14
+    elif x == 9:
+        return 31
+    elif x == 28:
+        return 84
+    elif x == 21:
+        return 42
+    elif x == 51:
+        return 67
+    elif x == 80:
+        return 99
+    elif x == 72:
+        return 91
+    else:
+        return x
