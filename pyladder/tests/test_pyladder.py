95a40517db4b312f3f85aadf8f3723b20c24beb6
Maram-Ankir <maramankir5@gmail.com>
2021-08-25 23:07:38 +0300

    Feat:testing files

--- pyladder/tests/test_pyladder.py
+++ pyladder/tests/test_pyladder.py
@@ -1,5 +1,8 @@
+import pyladder.pyladder.pyladder
 from pyladder import __version__
-
+feom pyladder.pyladder.pyladder.playing()
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_random_dice():
