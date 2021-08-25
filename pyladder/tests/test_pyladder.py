12f4560e460f98f701ed298e05191f650a713a04
Maram-Ankir <maramankir5@gmail.com>
2021-08-26 02:49:00 +0300

    feat:edit answers

--- pyladder/tests/test_pyladder.py
+++ pyladder/tests/test_pyladder.py
@@ -1,8 +1,9 @@
-import pyladder.pyladder.pyladder
+from pyladder import *
 from pyladder import __version__
-feom pyladder.pyladder.pyladder.playing()
 
 def test_version():
     assert __version__ == '0.1.0'
 
 def test_random_dice():
+    playing()
+   pass
